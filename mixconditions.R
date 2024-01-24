74f8e5328a84adf82f69e6b37e4854226ee8e159
Melissa (Millie) Johnston <73096271+millie-john@users.noreply.github.com>
2024-01-24 09:34:47 +0100

    Update mixconditions.R
    - Now tests for congruent and non-congruent
    - Added nicer reporting while running

--- mixconditions.R
+++ mixconditions.R
@@ -1,6 +1,6 @@
 # Generate 60 trial sheets
 # Written by Millie Johnston
-# Version 0.2
+# Version 0.3
 # Last updated 2023-01-016
 
 ### Randomise hand movements
@@ -10,14 +10,14 @@
 # Reward   R or L (no more than 2 in a row and equal distribution)
 # Movement P or C (no more than 2 in a row and equal distribution)
 # Final    R or L (no more than 2 in a row and equal distribution)
-# 
+#
 # Generate 3 sheets, then add labels 1,2,3, and then check for distribution again (no more than 2 in a row and equal distribution)
 # Individual sheets are named `Iteration_<number>.csv`
 # Final sheet is named `List_<number>.csv`
 # Sheets will be generated in the folder location you are executing from, you can check this using `getwd()`
 # You can change your working directory with `setwd()`, for example, `setwd("~/Desktop")`
 #
-# Packages are listed at the top and should autoinstall if they are not already available.
+# Packages are listed at the top and should auto-install if they are not already available.
 ##-----------------------------------------------
 
 # Install packages if not already installed
@@ -50,7 +50,7 @@ finpos <- apply(comb[, 1:2], 1, function(x)
 finpos <-
   ifelse(finpos == "LP", "L", ifelse(finpos == "RP", "R", ifelse(finpos == "LC", "R", "L")))
 comb <- cbind(comb, finpos)
-comb <- comb[rep(seq_len(nrow(comb)), 5),]
+comb <- comb[rep(seq_len(nrow(comb)), 5), ]
 
 # Make a list for sample hand
 sam <- list(sample = c("R", "L"))
@@ -62,7 +62,7 @@ sam <- expand.grid(sam)
 sam <- cbind(sam)
 
 # Replicate sample hand 10 times so it now equals 20
-sam <- sam[rep(seq_len(nrow(sam)), 10),]
+sam <- sam[rep(seq_len(nrow(sam)), 10), ]
 
 # Function to check if conditions are represented n or more times in a row, returns true of false
 has_consecutive <- function(vec, n) {
@@ -75,7 +75,7 @@ j = 1 # Start of counter so you can stop it after generating "sheets" lists
 df_comp <- data.frame() # Empty data frame to bind the lists
 
 # Loop to find data frames that pass criteria
-for (i in 1:500000) {
+for (i in 1:5000000) {
   # Make a random data frame of 20 trials sampled from list, sample without replacement
   df <- comb %>% slice_sample(n = 20, replace = FALSE)
   
@@ -91,16 +91,19 @@ for (i in 1:500000) {
     next
   }
   
-  # Nested loop to add sample hand, runs check to see if there are any combinations of 3 or more for the first 2 columns (sample and reward)
-  for (z in 1:100000) {
+  # Nested loop to add sample hand, runs check to see if there are any runs of 3 or more for the first 2 columns (sample and reward) and if there is an even distribution of congruent and noncongruent trials
+  for (z in 1:5000000) {
     df_s <- as.data.frame(sam) %>% slice_sample(n = 20, replace = FALSE)
     df_s <- cbind(df_s, df)
     if (any(apply(df_s[, 1:2], 2, function(x)
-      has_consecutive(x, 3)))) {
+      has_consecutive(x, 3))) ||
+      !all(table(paste(df_s[, 1], df_s[, 2], sep = "")) == 5)) {
       next
+    } else {
+      break
     }
-    break
   }
+  
   
   # Bind to df and add column names
   df <- cbind(df_s[, 1], df)
@@ -149,17 +152,18 @@ for (i in 1:500000) {
 
 df_copy <- df_comp
 
-new_df <- df_copy[1:2, ]
-df_copy <- df_copy[-(1:2), ]
+new_df <- df_copy[1:2,]
+df_copy <- df_copy[-(1:2),]
 
 rs_new_df <- new_df
 rs_df_copy <- df_copy
 
 i = 1
 
+print("Solving conditions...")
+
 # Start a while loop - this will run until it finds a solution
 while (nrow(df_copy) > 0) {
-  
   # Make row of interest
   roi <- df_copy %>% slice_sample(n = 1, replace = TRUE)
   
@@ -170,18 +174,15 @@ while (nrow(df_copy) > 0) {
     i = 1
     new_df <- rs_new_df
     df_copy <- rs_df_copy
-  } else {
-    print("Looking...")
   }
   
   # Make temp df of final three rows for check for consecutive
   temp <-
-    as.data.frame(rbind(new_df[(nrow(new_df) - 1):(nrow(new_df)), ], roi))
+    as.data.frame(rbind(new_df[(nrow(new_df) - 1):(nrow(new_df)),], roi))
   
   if (!any(apply(temp[, 1:5], 2, function(x)
     # Check for consecutive
     has_consecutive(x, 3)))) {
-    
     # If pass, bind new roi
     new_df <- rbind(new_df, roi)
     
@@ -193,14 +194,15 @@ while (nrow(df_copy) > 0) {
               df_copy[, 3],
               df_copy[, 4],
               df_copy[, 5]) %in% paste0(rownames(roi), roi[1, 1], roi[1, 2], roi[1, 3], roi[1, 4], roi[1, 5])
-    df_copy <- df_copy[death, ]
+    df_copy <- df_copy[death,]
     
   }
   
 }
 
 # Add tick boxes and column names
-df_save <- cbind(c(1:20,1:20,1:20), new_df, "[  ]", "[  ]", "", "[  ]", "")
+df_save <-
+  cbind(c(1:20, 1:20, 1:20), new_df, "[  ]", "[  ]", "", "[  ]", "")
 colnames(df_save) <-
   c("Trial",
     "Sample",
@@ -214,7 +216,7 @@ colnames(df_save) <-
     "✓",
     "")
 
-df_save <- df_save[,c(1,6,2,3,4,5,7,8,9,10)]
+df_save <- df_save[, c(1, 6, 2, 3, 4, 5, 7, 8, 9, 10)]
 
 # Write a csv with 60 trials that were randomised
 write.csv(
