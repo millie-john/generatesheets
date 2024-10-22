a2fa20ae387a8442cffc86742c21e2c480536e01
Melissa (Millie) Johnston <73096271+millie-john@users.noreply.github.com>
2024-10-22 20:07:58 +0200

    Update mixedconditions_24.R

--- mixedconditions_24.R
+++ mixedconditions_24.R
@@ -57,17 +57,8 @@ finpos <-
 comb <- cbind(comb, finpos)
 comb <- comb[rep(seq_len(nrow(comb)), 2), ] # Adjusted for 8 total
 
-# Make a list for sample hand
-sam <- list(sample = c("R", "L"))
-
-# Make a grid of possible sample hand combinations
-sam <- expand.grid(sam)
-
 # Make into a data frame for easy replication and binding
-sam <- cbind(sam)
-
-# Replicate sample hand 10 times so it now equals 20
-sam <- sam[rep(seq_len(nrow(sam)), 4), ]
+sam <- cbind(c("R","R","R","R","L","L","L","L"))
 
 # Function to check if conditions are represented in a sequence n or more times, in a column, returns true or false. Queries if the sequence "has consecutive", if false, it's a good thing.
 has_consecutive <- function(vec, n) {
@@ -80,34 +71,13 @@ j = 1 # Start of counter so you can stop it after generating "sheets" lists
 df_comp <- data.frame() # Empty data frame to bind the lists
 
 # Loop to find data frames that pass criteria
-for (i in 1:5000000) {
+for (i in 1:500) {
   # Make a random data frame of 20 trials sampled from list, sample without replacement
-  df <- comb %>% slice_sample(n = 8, replace = FALSE)
+  df <- comb
   
-  # Check if sample, reward, or movement is represented equals 10, retures true of false
-  if (any(apply(t(df), 1, function(x)
-    any(!table(x) == 4)))) {
-    next
-  }
-  
-  # Check is any value in any column is consecutive more than > or = 3 times, returns true of false
-  if (any(apply(df[, 1:3], 2, function(x)
-    has_consecutive(x, 3)))) {
-    next
-  }
-  
-  # Nested loop to add sample hand, runs check to see if there are any runs of 3 or more for the first 2 columns (sample and reward) and if there is an even distribution of congruent and noncongruent trials, and if there is an even distribution of the first 3 columns.
-  for (z in 1:5000000) {
-    df_s <- as.data.frame(sam) %>% slice_sample(n = 8, replace = FALSE)
-    df_s <- cbind(df_s, df)
-    if (any(apply(df_s[, 1:2], 2, function(x)
-      has_consecutive(x, 3))) ) {
-      next
-    } else {
-      break
-    }
-  }
-  
+  df_s <- as.data.frame(sam)
+  df_s <- cbind(df_s, df)
+
   # Five of the different types (RR, LL, RL, & LR)
   
   # Bind to df and add column names
